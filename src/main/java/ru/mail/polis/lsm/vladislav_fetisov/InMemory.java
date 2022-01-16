8feb8472141912cee342c123acdcb55e653b77fc
VladislavFetisov <fetisov2.vv@edu.spbstu.ru>
2022-01-16 19:14:35 +0300

    persistent first try

--- src/main/java/ru/mail/polis/lsm/vladislav_fetisov/InMemory.java
+++ src/main/java/ru/mail/polis/lsm/vladislav_fetisov/InMemory.java
@@ -2,6 +2,12 @@ package ru.mail.polis.lsm.vladislav_fetisov;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.util.Iterator;
 import java.util.NavigableMap;
 import java.util.SortedMap;
@@ -10,10 +16,45 @@ import java.util.concurrent.ConcurrentSkipListMap;
 import javax.annotation.Nullable;
 
 import ru.mail.polis.lsm.DAO;
+import ru.mail.polis.lsm.DAOConfig;
 import ru.mail.polis.lsm.Record;
 
 public class InMemory implements DAO {
+    private final String FILE_NAME = "DAO";
+    private final String TEMP_NAME = "TEMP";
     private final NavigableMap<ByteBuffer, Record> storage = new ConcurrentSkipListMap<>();
+    private final DAOConfig config;
+
+    public InMemory(DAOConfig config) {
+        this.config = config;
+        Path path = config.getDir().resolve(FILE_NAME);
+        if (Files.exists(path)) {
+            ByteBuffer length = ByteBuffer.allocate(Integer.BYTES);
+            try (FileChannel channel = FileChannel.open(path, StandardOpenOption.READ)) {
+                channel.read(length);
+                length.position(0);
+                ByteBuffer tempStorage = ByteBuffer.allocate(length.getInt());
+                channel.read(tempStorage);
+                tempStorage.position(0);
+                while (tempStorage.hasRemaining()) {
+                    ByteBuffer key = read(tempStorage);
+                    ByteBuffer value = read(tempStorage);
+                    storage.put(key, Record.of(key, value));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private ByteBuffer read(ByteBuffer from) {
+        int length = from.getInt();
+        ByteBuffer res = ByteBuffer.allocate(length);
+        for (int i = 0; i < length; i++) {
+            res.put(from.get());
+        }
+        return res.position(0);
+    }
 
     @Override
     public Iterator<Record> range(@Nullable ByteBuffer fromKey, @Nullable ByteBuffer toKey) {
@@ -39,7 +80,31 @@ public class InMemory implements DAO {
 
     @Override
     public void close() throws IOException {
+        Path path = config.getDir().resolve(FILE_NAME);
+        Path temp = config.getDir().resolve(TEMP_NAME);
+        ByteBuffer forLength = ByteBuffer.allocate(Integer.BYTES);
+        int length = 0;
+        try (FileChannel channel = FileChannel.open(temp, StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE)) {
+            for (Record record : storage.values()) {
+                length += record.getKey().remaining() + record.getValue().remaining() + 2 * Integer.BYTES;
+            }
+            forLength.putInt(length);
+            forLength.position(0);
+            channel.write(forLength);
+            for (Record record : storage.values()) {
+                write(record.getKey(), channel, forLength);
+                write(record.getValue(), channel, forLength);
+            }
+        }
+        Files.move(temp, path, StandardCopyOption.ATOMIC_MOVE);
+    }
 
+    private void write(ByteBuffer value, WritableByteChannel channel, ByteBuffer forLength) throws IOException {
+        forLength.position(0);
+        forLength.putInt(value.remaining());
+        forLength.position(0);
+        channel.write(forLength);
+        channel.write(value);
     }
 
     private SortedMap<ByteBuffer, Record> subMap(@Nullable ByteBuffer fromKey, @Nullable ByteBuffer toKey) {
